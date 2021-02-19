8ab94f2ee9e698ec7687e63bdac0cc5421cd29a9
unknown <idestura35@gmail.com>
2021-02-19 11:10:38 +0800

    fourth revision

--- src/Pages/AboutMe.js
+++ src/Pages/AboutMe.js
@@ -12,7 +12,7 @@ function AboutMe() {
         <div class="about__panelAbout">
             <animated.img style={img} src="about_me.png" alt=""/>
             <div class="about__textAbout">
-                <animated.h1 style={h1}>I'm Web/Mobile Developer</animated.h1>
+                <animated.h1 style={h1}>I'm a Web/Mobile Developer</animated.h1>
                 <animated.p style={p}>
                 I'm Ian C. Destura an IT student, one of my skill is programming. I'm interested in building websites and mobile development also in designing with touch of modern arts.
                 </animated.p>
